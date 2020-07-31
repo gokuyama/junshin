ff36500550f79825b71c036b7e0fb553e6ebe116
Gilberto Okuyama <gilberto.okuyama@serpro.gov.br>
2020-07-31 07:18:20 -0300

    Correção para mostrar a matricula quando a data fim é maior que a data atual

--- app/Http/Controllers/TurmaController.php
+++ app/Http/Controllers/TurmaController.php
@@ -40,7 +40,12 @@ class TurmaController  extends Controller
             ->where('alunos.ativo', 1)
             ->where('turmas.turma_id', $turma_id)
             ->whereDate('matricula_data_ini', '<=', Carbon::now()->toDateString())
-            ->whereNull('matricula_data_fim')
+            ->where(
+                function ($query) {
+                    $query->where('matricula_data_fim', '>', Carbon::now()->toDateString())
+                        ->orWhereNull('matricula_data_fim');
+                }
+            )
             ->orderBy('alunos.aluno_nome')
             ->get();
 
